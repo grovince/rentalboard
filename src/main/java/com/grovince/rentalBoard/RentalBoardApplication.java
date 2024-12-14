643a2289168b63db47b5df675a6942e2e62af924
grovince <grovinceln@gmail.com>
2024-12-15 00:55:30 +0900

    [feat] 백엔드 초기 설정

---
+++ src/main/java/com/grovince/rentalBoard/RentalBoardApplication.java
@@ -0,0 +1,13 @@
+package com.grovince.rentalBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grovince/rentalBoard/RentalBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grovince.rentalBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
